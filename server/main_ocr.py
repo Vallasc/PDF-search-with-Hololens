0f3cbd1a2ebb986b072a2d190ad4ede50dc2a025
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2022-05-05 16:28:51 +0200

    Update

--- server/main_ocr.py
+++ server/main_ocr.py
@@ -1,6 +1,6 @@
-import os
 import easyocr
-import cv2
+
+from bottle import route, request, run, static_file
 
 IMAGE_PATH = 'https://blog.aspose.com/wp-content/uploads/sites/2/2020/05/Perform-OCR-using-C.jpg'
 reader = easyocr.Reader(['en'])
