d05552865b8c512fd20f5cd1fad72cc4e63cf3f5
Giacomo Vallorani <giacomovallorani.97@gmail.com>
2022-05-05 17:53:09 +0200

    Add web server

--- server/main_ocr.py
+++ server/main_ocr.py
@@ -1,8 +1,41 @@
 import easyocr
+import os
+import json
+import time as time_
+import numpy as np
 
-from bottle import route, request, run, static_file
+from bottle import route, request, run, static_file, FileUpload
 
-IMAGE_PATH = 'https://blog.aspose.com/wp-content/uploads/sites/2/2020/05/Perform-OCR-using-C.jpg'
 reader = easyocr.Reader(['en'])
-result = reader.readtext(IMAGE_PATH)
-print (result)
+
+
+def millis():
+    return int(round(time_.time() * 1000))
+    
+def convert(o):
+    if isinstance(o, np.generic): return o.item()  
+    else: return o
+
+@route('/')
+def root():
+    return static_file('./public/test_upload.html', root='.')
+
+
+@route('/upload', method='POST')
+def do_upload():
+    upload :FileUpload = request.files.get('snapshot')
+    name, ext = os.path.splitext(upload.filename)
+    if ext not in ('.png', '.jpg', '.jpeg'):
+        return "File extension not allowed."
+
+    save_path = "./tmp"
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    file_path = "{path}/{file}".format(path=save_path, file=millis())
+    upload.save(file_path)
+    result = reader.readtext(file_path)
+    return json.dumps(result, default=convert)
+
+if __name__ == '__main__':
+    run(host='localhost', port=9999)
