73f00c3c55686d03a251d600f7981437165688c4
Vallasc <giacomovallorani.97@gmail.com>
2022-12-29 19:34:11 +0100

    Bugfix

--- server/pdf_utils.py
+++ server/pdf_utils.py
@@ -3,8 +3,9 @@ import fitz
 import string
 import base64
 import json
-from PIL import Image
+from PIL import Image, ImageDraw
 from io import BytesIO
+from copy import deepcopy
 
 
 class PdfUtils:
@@ -12,6 +13,10 @@ class PdfUtils:
     @staticmethod
     def absolutePath(path):
         return os.path.abspath(os.path.expanduser(os.path.expandvars(path)))
+
+    @staticmethod
+    def sanitize_word(word):
+        return word.lower().translate(str.maketrans('', '', string.punctuation))
 
     # List all pdfs in the directory
     @staticmethod
@@ -31,6 +36,19 @@ class PdfUtils:
         return out
 
     @staticmethod
+    def select_keyword_on_image(page_keyword):
+        with open(page_keyword['path'], encoding = 'utf-8') as f:
+            data = json.load(f)
+        with Image.open(BytesIO(base64.b64decode(data['img']))) as im:
+            for box in page_keyword['keywords']:
+                draw = ImageDraw.Draw(im)
+                draw.rectangle([box['x0'], box['y0'], box['x1'], box['y1']], outline=(255, 0, 0, 255), fill=None, width=2)
+            out_bytes = BytesIO()
+            im.save(out_bytes, format="JPEG")
+        data['img'] = base64.b64encode(out_bytes.getvalue()).decode("utf-8")
+        return data
+
+    @staticmethod
     def extract_pdf_info(pdf, pdfs, words, keywords, destination):
         destination = destination + os.sep + pdf["name"]
         if not os.path.exists(destination):
@@ -41,16 +59,28 @@ class PdfUtils:
         num_pages = 0
         # foreach page in pdf
         for idx, page in enumerate(doc):
+            real_page_size = (page.rect.width, page.rect.height)
             pix = page.get_pixmap(dpi=100) 
             the_page_bytes = pix.pil_tobytes(format="JPEG")
             page_file = "page-%s.json" % idx
             page_path = PdfUtils.absolutePath(destination +  os.sep + page_file)
             # Save page image as json and base64
             image_bytes = BytesIO(the_page_bytes)
-            image = Image.open(image_bytes)
-            width, height = image.size
-            img_str = base64.b64encode(image_bytes.getvalue())
-            out = { "img": img_str.decode("utf-8") , "width": width, "height": height}
+            with Image.open(BytesIO(the_page_bytes)) as image:
+                image_size = image.size
+                # Save page as base64 string
+                img_str = base64.b64encode(image_bytes.getvalue()).decode("utf-8")
+                # Save thumbnail of page
+                thumbnail_bytes = BytesIO()
+                image.thumbnail((300, 300))
+                image.save(thumbnail_bytes, format="JPEG")
+            thumbnail_str = base64.b64encode(thumbnail_bytes.getvalue()).decode("utf-8")
+            out = { 
+                "img": img_str,
+                "thumbnail": thumbnail_str,
+                "width": image_size[0], 
+                "height": image_size[1]
+            }
             with open(page_path, "w") as outf:
                 json.dump(out, outf)
 
@@ -60,23 +90,18 @@ class PdfUtils:
             out_page["pdfId"] = pdf["name"]
             out_page["path"] = page_path
             out_page["url"] = "/pdfs/%s/%s" % (pdf["name"], page_file)
-            # out_page["keywords"] = []
-            # pdf["pages"].append(out_page)
-
             for w in page.get_text("words"):
-                word = w[4].lower().translate(str.maketrans('', '', string.punctuation))
+                word = PdfUtils.sanitize_word(w[4])
                 if word not in keywords:
                     continue
                 word_obj = { 
-                    "x0": w[0], 
-                    "y0": w[1],
-                    "x1": w[2], 
-                    "y1": w[3], 
+                    "x0": int(w[0] * image_size[0] / real_page_size[0]), 
+                    "y0": int(w[1] * image_size[1] / real_page_size[1]),
+                    "x1": int(w[2] * image_size[0] / real_page_size[0]), 
+                    "y1": int(w[3] * image_size[1] / real_page_size[1]),
                     # Remove punctuation
                     "word": word
                 }
-                if word_obj["word"] == "": continue
-
                 # Create word if not exists
                 if word not in words:
                     words[word] = {
@@ -87,15 +112,15 @@ class PdfUtils:
                 word_pdfs = words[word]
                 # Create pdf if not exists
                 if pdf["_id"] not in word_pdfs["pdfs"]:
-                    word_pdfs["pdfs"][pdf["_id"]] = pdf.copy()
+                    word_pdfs["pdfs"][pdf["_id"]] = deepcopy(pdf)
+                    word_pdfs["pdfs"][pdf["_id"]]['pages'] = []
                     word_pdfs["pdfs"][pdf["_id"]]["numOccKeyword"] = 0
                 word_pdfs["pdfs"][pdf["_id"]]["numOccKeyword"] += 1
 
                 word_pdf_pages = word_pdfs["pdfs"][pdf["_id"]]["pages"]
-
                 filtered = [p for p in word_pdf_pages if p["number"] == idx]
                 if len(filtered) == 0:
-                    tmp = out_page.copy()
+                    tmp = deepcopy(out_page)
                     tmp["keywords"] = [word_obj]
                     word_pdf_pages.append(tmp)
                 else:
